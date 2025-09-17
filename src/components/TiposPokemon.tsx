c0bbd68bfe3842547774575885d69e63babac7e5
Guillermo Antonio Ortiz Corella <gaoc117@gmail.com>
2025-09-16 21:36:30 -0600

    corregido click
    se corrigen los clicks

--- src/components/TiposPokemon.tsx
+++ src/components/TiposPokemon.tsx
@@ -16,7 +16,7 @@ const handleClickTipo = (tipo: string) => {
   return (
 
    
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-2 w-full px-10 mb-30 md:mb-10">
+    <div className="z-10 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-2 w-full px-10 mb-30 md:mb-10">
       {tiposPokemon.results?.map((tipo) => (
         <button
           onClick={() => handleClickTipo(tipo.name)}
